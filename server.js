d0c4693fd5459fe43265d45edd0e6072d2962cc9
Afraz Malik <afrazmalik321@gmail.com>
2021-04-26 20:31:35 +0500

    'ok'

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image.js');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
+    connectionString : 'postgresql-convex-01478',
     ssl: true
   }
 });
