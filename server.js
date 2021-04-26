f9ca67745bef6d64de023afd45d2723c46747f14
Afraz Malik <afrazmalik321@gmail.com>
2021-04-26 19:43:00 +0500

    'ok'

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image.js');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'localhost',	
-    user : 'postgres',
-    password : 'root',
-    database : 'smartbrain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
