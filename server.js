b8cfe6e450b7f1cefad71b995e03a80ee2214363
Afraz Malik <afrazmalik321@gmail.com>
2021-07-24 22:02:56 +0500

    'ok'

--- server.js
+++ server.js
@@ -35,24 +35,29 @@ const databaseheroku = knex({
 const db = databaseheroku
 
 const app = express()
-app.use(bodyParser.json())
-app.use(cors())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+
   next()
 })
+app.use(bodyParser.json())
+app.use(cors())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')))
 
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
-}
+//   app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+//   })
+// }
 
-// app.get('/', (req, res) => {
-//   res.json('PostgreSql from github')
-// })
+app.get('/', (req, res) => {
+  res.json('PostgreSql from github')
+})
 app.get('/admin', (req, res) => admin.adminHandler(req, res, db))
 app.post('/signin', (req, res) => signin.signInHandler(req, res, db, bcrypt))
 app.post('/register', (req, res) =>
